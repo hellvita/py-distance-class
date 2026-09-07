6553f086fcae3813caf428ddfdc1d5effeba96db
hellvita <seredaolga03@gmail.com>
2026-09-07 16:17:09 +0300

    clean-up: remove raise TypeError

--- app/main.py
+++ app/main.py
@@ -12,70 +12,43 @@ class Distance:
         return f"Distance(km={self.km})"
 
     def __add__(self, other: Distance | int | float) -> Distance:
-        if isinstance(other, int | float):
-            return Distance(self.km + other)
-        elif isinstance(other, Distance):
+        if isinstance(other, Distance):
             return Distance(self.km + other.km)
-        else:
-            raise TypeError("other must be a Distance object or an integer")
+        return Distance(self.km + other)
 
     def __iadd__(self, other: Distance | int | float) -> Distance:
-        if isinstance(other, int | float):
-            self.km += other
-        elif isinstance(other, Distance):
-            self.km += other.km
-        else:
-            raise TypeError("other must be a Distance object or an integer")
+        if isinstance(other, Distance):
+            other = other.km
+        self.km += other
         return self
 
     def __mul__(self, other: int | float) -> Distance:
-        if isinstance(other, int | float):
-            return Distance(self.km * other)
-        else:
-            raise TypeError("other must be a Distance object or an integer")
+        return Distance(self.km * other)
 
     def __truediv__(self, other: int | float) -> Distance:
-        if isinstance(other, int | float):
-            return Distance(round(self.km / other, 2))
-        else:
-            raise TypeError("other must be a Distance object or an integer")
+        return Distance(round(self.km / other, 2))
 
     def __lt__(self, other: Distance | int | float) -> bool:
-        if isinstance(other, int | float):
-            return self.km < other
-        elif isinstance(other, Distance):
+        if isinstance(other, Distance):
             return self.km < other.km
-        else:
-            raise TypeError("other must be a Distance object or an integer")
+        return self.km < other
 
     def __gt__(self, other: Distance | int | float) -> bool:
-        if isinstance(other, int | float):
-            return self.km > other
-        elif isinstance(other, Distance):
+        if isinstance(other, Distance):
             return self.km > other.km
-        else:
-            raise TypeError("other must be a Distance object or an integer")
+        return self.km > other
 
     def __eq__(self, other: Distance | int | float) -> bool:
-        if isinstance(other, int | float):
-            return self.km == other
-        elif isinstance(other, Distance):
+        if isinstance(other, Distance):
             return self.km == other.km
-        else:
-            raise TypeError("other must be a Distance object or an integer")
+        return self.km == other
 
     def __le__(self, other: Distance | int | float) -> bool:
-        if isinstance(other, int | float):
-            return self.km <= other
-        elif isinstance(other, Distance):
+        if isinstance(other, Distance):
             return self.km <= other.km
-        else:
-            raise TypeError("other must be a Distance object or an integer")
+        return self.km <= other
 
     def __ge__(self, other: Distance | int | float) -> bool:
-        if isinstance(other, int | float):
-            return self.km >= other
-        elif isinstance(other, Distance):
+        if isinstance(other, Distance):
             return self.km >= other.km
-        else:
-            raise TypeError("other must be a Distance object or an integer")
+        return self.km >= other
